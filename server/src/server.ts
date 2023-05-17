a460b0590aca7e4765c036d164fef7dd86ac4146
Juliano Moser <juliano.moserjj@gmail.com>
2023-05-17 14:46:31 -0300

    Implementado cors (server)

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,12 @@
 import fastify from 'fastify'
+import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
+
+app.register(cors, {
+  origin: true,
+})
 
 app.register(memoriesRoutes)
 
@@ -10,5 +15,5 @@ app
     port: 3333,
   })
   .then(() => {
-    console.log('🚀 HTTP server running on http://localhost:3333')
+    console.log('🚀 HTTP server running on port http://localhost:3333')
   })
